b5ea0e687fe7f7dc3f0de3a96cf7d9218df3e493
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2024-12-10 16:38:04 -0500

    Add files via upload

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import { Link as LinkScroll } from 'react-scroll';
 
 const NavLink = ({ title }) => (
@@ -15,15 +16,18 @@ const NavLink = ({ title }) => (
 );
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+
     return (
         <header className="fixed top-0 left-0 z-50 w-full py-10">
             <div className="container flex h-14 items-center max-lg:px-5">
                 <a className="lg:hidden flex-1 cursor-pointer z-2">
                     <img src="/images/xora.svg" width={115} height={55} alt="logo" />
                 </a>
-
-                <div className="w-full border-2 border-amber-400">
-                    <nav>
+                <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+                <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+                    <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
                         <ul className="flex max-lg:block max-lg:px-12">
                             <li className="nav-li">
                             <NavLink title="features" />
@@ -31,15 +35,32 @@ const Header = () => {
                             <NavLink title="pricing" />
                             </li>
 
-                            <li>
+                            <li className="nav-logo">
                                 <LinkScroll>
-                                    <img src="/images/xora.svg" width={160} height={55} alt="logo"/>
+                                    <img src="/images/xora.svg" width={160} height={55} alt="logo" />
 
                                 </LinkScroll>
+                            </li>
+
+                            <li className="nav-li">
+                                <NavLink title="faq" />
+                                <div className="dot" />
+                                <NavLink title="download" />
                             </li>
                         </ul>
                     </nav>
                 </div>
+            </div>
+
+            <button className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
+                    onClick={() => setIsOpen((prevState) => !prevState
+            )}>
+                <img
+                    src={`/images/${isOpen ? "close" : "magic"}magic.svg`}
+                    alt="magic"
+                    className="size-1/2 object-contain"
+                />
+            </button>
             </div>
         </header>
     )
